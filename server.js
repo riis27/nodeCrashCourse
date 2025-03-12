a46e69cf80467645a06580910d63992c136bb199
rissa <rissasanmiguel@gmail.com>
2025-03-12 00:24:14 -0400

    homework update

---
+++ server.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method)
+
+    //set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    let path = './views/';
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            path += 'about.html';
+            res.
+            res.setHeader('Location', '/about');
+            res.end();
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    }
+
+    //send an html file
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.write(data);
+            res.end();
+        }
+    })
+
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('listening for requests on  port 3000');
+})
